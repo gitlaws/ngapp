5ec964ec079ac3717df257d88224497f918d8e0c
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-02-04 19:18:20 -0500

    commit 1

--- src/app/components/banner-cut-out/banner-cut-out.component.ts
+++ src/app/components/banner-cut-out/banner-cut-out.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-banner-cut-out',
-  templateUrl: './banner-cut-out.component.html',
-  styleUrls: ['./banner-cut-out.component.scss']
+  selector: "app-banner-cut-out",
+  templateUrl: "./banner-cut-out.component.html",
+  styleUrls: ["./banner-cut-out.component.scss"],
 })
 export class BannerCutOutComponent {
-  @Input() public imageUrl = 'assets/back-1.jpg';
-  @Input() public title = '100 Angular Challenge';
+  @Input() public imageUrl = "assets/back-1.jpg";
+  @Input() public title = "100 Angular Challenge";
 }
